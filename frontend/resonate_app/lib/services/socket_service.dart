aa591340bebaa37eaf2afcf81962a65d1b50a3fd
Ruchir Sharma <ruchirsharma074@gmail.com>
2026-03-27 15:47:39 +0530

    Added basic pig-pong for finding rtt and offset

--- frontend/resonate_app/lib/services/socket_service.dart
+++ frontend/resonate_app/lib/services/socket_service.dart
@@ -1,8 +1,13 @@
 // ignore: library_prefixes
 import 'package:socket_io_client/socket_io_client.dart' as IO;
+//import 'dart:async';
 
 class SocketService {
   static final SocketService _instance = SocketService._internal();
+  double smoothedOffset = 0;
+  bool initialized = false;
+
+  Stopwatch stopwatch = Stopwatch();
 
   factory SocketService() {
     return _instance;
@@ -23,10 +28,14 @@ class SocketService {
 
     socket.onConnect((_) {
       //print("Connected to server");
+      startInitialSync();
     });
 
     socket.onDisconnect((_) {
       //print("Disconnected")
+    });
+    socket.on('pong', (data) {
+      handlePong(data);
     });
   }
 
@@ -40,5 +49,49 @@ class SocketService {
 
   void disconnect() {
     socket.disconnect();
+  }
+
+  void startInitialSync() async {
+    for (int i = 0; i < 5; i++) {
+      await Future.delayed(Duration(milliseconds: 50 + (i * 20)));
+      sendPing();
+    }
+  }
+
+  void sendPing() {
+    final t0 = DateTime.now().millisecondsSinceEpoch;
+    stopwatch.reset();
+    stopwatch.start();
+    socket.emit('ping', {'t0': t0});
+  }
+
+  void handlePong(dynamic data) {
+    stopwatch.stop();
+
+    final t3 = DateTime.now().millisecondsSinceEpoch;
+    final t0 = data['t0'];
+    final t1 = data['t1'];
+    final t2 = data['t2'];
+
+    final rtt = stopwatch.elapsedMilliseconds.toDouble();
+
+    final offset = ((t1 - t0) + (t2 - t3)) / 2;
+    updateSync(rtt, offset);
+  }
+
+  void updateSync(double rtt, double offset) {
+    if (rtt > 300) return;
+
+    if (!initialized) {
+      initialized = true;
+      smoothedOffset = offset;
+    } else {
+      smoothedOffset = 0.1 * offset + 0.9 * smoothedOffset; //alpha = 1
+    }
+  }
+
+  double getServerTime() {
+    final clientTime = DateTime.now().millisecondsSinceEpoch;
+    return clientTime + smoothedOffset;
   }
 }
